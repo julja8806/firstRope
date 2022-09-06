5d9a3b8b53b32df8059d1961b0fee6cba3206757
julja8806 <112933797+julja8806@users.noreply.github.com>
2022-09-06 11:53:26 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,5 +2,8 @@ public class Main {
   public static void main(String[] args){
     int a = 10;
     int b = 20;
-    int result = a + b;
-    System.out.println(result)
+    int c = 30;
+    int result = a + b + c;
+    System.out.println(result);
+    }
+}
